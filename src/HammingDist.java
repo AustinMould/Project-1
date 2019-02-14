c376acc1b4b60881f790c6a997e1a443e9a6fdb3
Austin Mould <austin.k.mould-1@ou.edu>
2019-02-13 20:59:22 -0600

    readFile method completed and debugged

--- src/HammingDist.java
+++ src/HammingDist.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HammingDist {
 	
 	//private variables
@@ -7,13 +11,26 @@ public class HammingDist {
 	
 	private String secondStation;
 	
+	private String[] allStations = new String[119];
+	
 	//HammingDist Constructor which takes in two string inputs for city/station ID
 	public HammingDist(String firstStation, String secondStation) {
 		this.firstStation = firstStation;
 		this.secondStation = secondStation;
 		calcDistance();
+		
+		try
+    	{
+			read("Mesonet.txt");
+    	}
+    	catch(IOException e)
+    	{
+    		System.out.println("Error reading from file!\n");
+    		e.printStackTrace();
+    	}
 	}
 	
+	//Method for calculating Hamming distance between two IDs
 	public int calcDistance() {
 		distance = 0;
 		for (int index = 0; index < 4; ++index) {
@@ -24,6 +41,39 @@ public class HammingDist {
 			}
 		}
 		return distance;
+	}
+	
+	
+	//Reads file to String[] allStations
+	public void read(String filename) throws IOException
+    {
+    	// Use a buffered Reader on the file:
+    	BufferedReader br = new BufferedReader(new FileReader(filename));
+        String strg;
+        strg = br.readLine();
+        strg = br.readLine();
+        strg = br.readLine();
+        
+        for(int index = 0; index < allStations.length; ++index) {
+
+        	strg = br.readLine();
+        	if(strg == null) {
+        		break;
+        	}
+
+        	allStations[index] = strg;
+        }
+        
+        br.close();
+    }
+	
+	public int[] calcAllDistance(String station) {
+		
+		int[] allHammingDistanceCounts = new int[4];
+		
+		
+		
+		return null;
 	}
 	
 	/* Output for toString should be modeled like so:
